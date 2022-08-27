b350deb15008478421b383c507e72d6f8f92f355
ckavanattu <chackojk@gmail.com>
2022-08-26 20:47:35 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const db = require('./config/connection');
 const logger = require("morgan");
-const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 
 app.use(logger("dev"));
 
@@ -16,14 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+
 
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+})
